e47e83f80725dce336a369f5208bf40f4ac6df67
Ken Bingham <kbingh64@gmail.com>
2017-11-19 18:22:57 -0700

    adding domain classes

---
+++ src/main/java/com/stg/mymessage/domain/Message.java
@@ -0,0 +1,49 @@
+package com.stg.mymessage.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Message {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long messageId;
+	private String subject;
+	private String message;
+	private Long toUserId;
+	private Long fromUserId;
+	
+	public Long getMessageId() {
+		return messageId;
+	}
+	public void setMessageId(Long messageId) {
+		this.messageId = messageId;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public Long getToUserId() {
+		return toUserId;
+	}
+	public void setToUserId(Long toUserId) {
+		this.toUserId = toUserId;
+	}
+	public Long getFromUserId() {
+		return fromUserId;
+	}
+	public void setFromUserId(Long fromUserId) {
+		this.fromUserId = fromUserId;
+	}
+}
