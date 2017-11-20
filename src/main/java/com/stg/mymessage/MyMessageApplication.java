7bfb9c11ee812c3918056b3b9b668a3cb9686df9
Ken Bingham <kbingh64@gmail.com>
2017-11-19 17:33:00 -0700

    Initial Commit

---
+++ src/main/java/com/stg/mymessage/MyMessageApplication.java
@@ -0,0 +1,12 @@
+package com.stg.mymessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMessageApplication.class, args);
+	}
+}
---
+++ src/test/java/com/stg/mymessage/MyMessageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stg.mymessage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyMessageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
