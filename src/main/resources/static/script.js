f620c38fda7973b3665568c0464af8558f113995
Ken Bingham <kenneth.bingham@stgconsulting.com>
2018-03-05 09:20:19 -0700

    adding script js file and new Message page

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,79 @@
+$(function() {
+    $("#userForm").submit(function(event) {
+        // Prevent the form from submitting via the browser.
+        event.preventDefault();
+
+        var name = $("#userName").val();
+        $.ajax({
+            type: "POST",
+            url: "/addUser?userName=" + name,
+            dataType: "plain/text",
+            success: function (result) {
+
+                $(".list-group").append("<li>" + result.userName + "</li>");
+            },
+
+            data: name
+        });
+
+    });
+
+
+
+
+});
+
+function sendMessage(){
+    // Prevent the form from submitting via the browser.
+    event.preventDefault();
+
+    var sender = $("#messageSenderName").val();
+    var receiver = $('#messageReceiverName option:selected').text();
+    var message =  $("#messageBody").val();
+
+    var messageJson = {
+
+        "message": message,
+        "fromUserName": sender,
+        "toUserName": receiver
+    }
+    $.ajax({
+        type: "POST",
+        url: "/sendMessage",
+        contentType: "application/json",
+        data: messageJson,
+        success: function (result) {
+
+
+        }
+
+
+    });
+
+}
+
+
+function findAll() {
+
+    $.ajax({
+        type: "GET",
+        url: "/users",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            renderJsonList(result)
+        }
+    });
+}
+
+function renderJsonList(result){
+
+    $( "#allUserNamesList").empty();
+
+    $.each(result, function (id, user) {
+
+        $(".list-group").append("<li>" + user.userName + "</li>");
+
+    });
+
+}
